01e2e7e3dc59241b3bca137cf63f7d8d9e7c1542
mcantiere <matheus.cantieree@gmail.com>
2026-06-04 23:55:22 -0300

    feat: add docs layout

---
+++ src/lib/shared.ts
@@ -0,0 +1,10 @@
+export const appName = 'Neocontratos';
+export const docsRoute = '/documentacao';
+export const docsImageRoute = '/og/documentacao';
+export const docsContentRoute = '/llms.mdx/documentacao';
+
+export const gitConfig = {
+  user: 'mpereira',
+  repo: 'neocontratos',
+  branch: 'main',
+};
